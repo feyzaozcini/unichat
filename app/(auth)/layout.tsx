507e26992dbeb885f7673fba3f91f8ddb146bd07
feyzaozcini <feyza_ozcini@hotmail.com>
2024-03-03 15:00:48 +0300

    Clerk Added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const  AuthLayout= ({children}: {children: React.ReactNode}) => {
     return ( 
-        <div className="bg-red-500 h-full">
+        <div className="h-full flex items-center justify-center">
             {children}
         </div>
      );
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-import { Button } from "@/components/ui/button";
-
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-bold text-indigo-500">
-      Hello Unichat
-      </p>
-      <Button>
-        Tıkla
-        </Button>
-      </div>
-    
+      <UserButton
+        afterSignOutUrl="/"  
+      />
+    </div>
   );
 }
 
