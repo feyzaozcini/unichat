374a48fbbed3c7aed6416e3b255c08f74ad993da
edanurdemircii <ednrdmrc9@gmail.com>
2024-03-21 22:32:05 +0300

    Delete & Leave server modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,6 +6,8 @@ import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 
 export const ModalProvider = () => {
     const[isMounted, setIsMounted]=useState(false);
@@ -27,6 +29,8 @@ export const ModalProvider = () => {
         <EditServerModal />
         <MembersModal/>
         <CreateChannelModal />
+        <LeaveServerModal />
+        <DeleteServerModal/>
         </>
     )
 }
