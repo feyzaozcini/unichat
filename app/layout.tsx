ab36f595c69338338b8a253ffc2b32e7fad2f785
edanurdemircii <ednrdmrc9@gmail.com>
2024-04-04 16:16:49 +0300

    Socket IO setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { cn } from "@/lib/utils";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -27,9 +28,12 @@ export default function RootLayout({
            attribute="class"
            defaultTheme="dark"
            enableSystem={false}
-           storageKey="discord-theme">
-          <ModalProvider/>
-          {children}     
+           storageKey="discord-theme"
+          >
+           <SocketProvider>
+            <ModalProvider/>
+            {children}
+            </SocketProvider>            
         </ThemeProvider>  
         </body>
       </html>
