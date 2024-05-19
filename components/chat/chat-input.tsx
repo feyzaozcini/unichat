d7c2cc3dc98e7e34fccef0493192ec0c0bf68751
feyzaozcini <feyza_ozcini@hotmail.com>
2024-05-19 11:30:10 +0300

    autocomplete edited

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -84,9 +84,10 @@ export const ChatInput = ({
                     className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
                     placeholder={`Message ${type === "conversation" ? name : "#" + name}`}
                     {...field}
+                    autoComplete="off"  
                   />
                   <div className="absolute top-7 right-8">
-                  <EmojiPicker
+                    <EmojiPicker
                       onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
                     />
                   </div>
