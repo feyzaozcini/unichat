0345d16975536a47abed8feb8f041a36a9553275
edanurdemircii <ednrdmrc9@gmail.com>
2024-06-03 16:05:48 +0300

    edited

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -34,7 +34,7 @@ export const useChatQuery = ({ queryKey, apiUrl, paramKey, paramValue }: ChatQue
         queryKey: [queryKey],
         queryFn: fetchMessages,
         getNextPageParam: (lastPage) => lastPage?.nextCursor,
-        refetchInterval: isConnected ? false : 1000,
+        refetchInterval:  1000,
     });
 
     return {
